a66c16fd07b33622b44866078ab7d2c3f1d3b592
kadenshindan <kadenshindan@users.noreply.github.com>
2026-08-17 08:12:14 +0900

    replace sharp PJ-S3DS with successor PJ-U3DS on fan ranking

--- products.js
+++ products.js
@@ -1590,9 +1590,9 @@ const PRODUCTS = [
   {
     "cat": "fan",
     "maker": "シャープ",
-    "model": "PJ-S3DS",
+    "model": "PJ-U3DS",
     "name": "プラズマクラスター扇風機 DC",
-    "price": 14780,
+    "price": 14800,
     "size": [
       "s",
       "m"
@@ -1612,8 +1612,8 @@ const PRODUCTS = [
       "部屋干しの消臭にも活躍",
       "コスパの高い1.5万円以下"
     ],
-    "img": "https://tshop.r10s.jp/e-kurashi/cabinet/main-img/020/main-t0h59.jpg",
-    "rakuten": "https://hb.afl.rakuten.co.jp/hgc/5600500c.eee1bd55.5600500d.dffe8316/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ftatsumax%2Ftake-pjs3dsw%2F&m=https%3A%2F%2Fitem.rakuten.co.jp%2Ftatsumax%2Ftake-pjs3dsw%2F",
+    "img": "https://shop.r10s.jp/ei-one/cabinet/syouhin_seikatu/03265069/imgrc0137633654.jpg",
+    "rakuten": "https://hb.afl.rakuten.co.jp/hgc/5600500c.eee1bd55.5600500d.dffe8316/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fei-one%2Fpj-u3ds%2F&m=https%3A%2F%2Fitem.rakuten.co.jp%2Fei-one%2Fpj-u3ds%2F",
     "officialUrl": "https://jp.sharp/pci_fan/products/pj-u3ds/spec/"
   },
   {
